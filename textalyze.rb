935ee02ea5a4800905671a5e763c381cd7bb616e
John Davison <johncdavison@gmail.com>
2014-10-10 12:15:11 -0700

    Refactor V[1.2]

--- textalyze.rb
+++ textalyze.rb
@@ -26,6 +26,7 @@ end
 
 # END V[0.1]
 
+
 # BEGIN v[0.2]
 
 def to_chars(string)
@@ -33,6 +34,7 @@ def to_chars(string)
 end
 
 # END V[0.2]
+
 
 # BEGIN v[0.3]
 
@@ -93,34 +95,8 @@ end
 
 # END V[1.1]
 
-def contains_words?(phrase)
-  return false unless phrase.match(/([a-zA-Z])*\s+([a-zA-Z])+/)
-  true
-end
 
-def process_multiple_items(items)
-  results = {}
-  items.each do |item|
-    count_statistics = item_counts(arrayify_characters(item))
-    if results.empty?
-      results = count_statistics
-    else
-      results.merge!(count_statistics) do | key, oldvalue, newvalue |
-        oldvalue + newvalue
-      end
-    end
-  end
-  results
-end
-
-def process_from_file(file_name)
-  return "please enter a file name" unless file_name
-  lines = File.open(file_name, "r").each_with_object([]) do |line, object|
-    object.push line
-  end
-  process_multiple_items(lines)
-end
-
+# BEGIN V[1.2]
 
 def print_histogram(frequencies, char= nil, width = nil)
   width ||= 100; char ||= "*"
@@ -129,30 +105,9 @@ def print_histogram(frequencies, char= nil, width = nil)
   end
 end
 
-
-
-def relative_frequencies(counts)
-  return unless counts.respond_to?(:keys) && counts.values.all? {|n| n.respond_to? :+}
-  total_characters = counts.values.reduce(:+).to_f
-  relative_frequencies = counts.each_with_object({}) do |(key, value), object|
-    object[key] = (value / total_characters).round(3)
-  end
-end
+# END V[1.2]
 
 puts "WeLcOmE to Text.ly 2.0"
 if __FILE__ == $0
-  puts request_and_process_user_supplied_file()
-  # p item_counts([1,2,1,2,1]) == {1 => 3, 2 => 2}
-  # p item_counts(["a","b","a","b","A","ZZZ"]) == {"a" => 3, "b" => 2, "zzz" => 1}
-  # p item_counts([]) == nil
-  # p item_counts(["hi", "hi", "hi"]) == {"hi" => 3}
-  # p item_counts([true, nil, "dinosaur"]) == {true => 1, nil => 1, "dinosaur" => 1}
-  # p item_counts(["a","a","A","A"]) != {"a" => 2, "A" => 2}
-  # p sanitize("@jCDAvison") == "@jcdavison"
-  # sensible_print item_counts(["a","a","A","A", nil, true, false, true])
-  # p arrayify_characters("j\$jd") == [ "j", "\$", "j", "d"]
-  # sensible_print item_counts(arrayify_characters("joooohndaaaahvison"))
-  # p relative_frequencies({a: 3, b: 5, c: 1000}) == {:a=>0.003, :b=>0.005, :c=>0.992}
-  # p print_histogram(relative_frequencies({a: 3, b: 5, c: 10}),"8" , 25)
-  # sensible_print(process_from_file(ARGV[0]))
+  # What kind of 'rumble strips' make sense? 
 end
